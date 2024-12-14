06c25752481e4d110c12908334b00244202723bc
Evgeniy716 <zenkavolk8448@gmail.com>
2024-12-14 12:46:57 +0300

    correct 14.12.24
    correct 14.12.24 only model django

--- UrbanDjango/urls.py
+++ UrbanDjango/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from task2.views import task2_func_view, ViewByClass
 from task3.views import *
 from task4.views import *
-from task5.views import sign_up_by_django, sign_up_by_html
+from task5.views import sign_up_by_django
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('func_view/', task2_func_view),
@@ -11,8 +11,8 @@ urlpatterns = [
     path('platform/', game_platform),
     path('platform/games/', game),
     path('platform/cart/', cart),
-    path('', sign_up_by_html),
-    # path('', sign_up_by_django)
+    # path('', sign_up_by_html),
+    path('', sign_up_by_django)
 ]
 
 
--- task5/views.py
+++ task5/views.py
@@ -4,51 +4,50 @@ from .forms import ContactForm
 
 
 
-def sign_up_by_html(request):
-    users = {
-        'Ivan': 'Ivan1999','Petr': 'Petya2005'
-    }
-    info = {
-    }
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        repeat_password = request.POST.get("repeat_password")
-        age = request.POST.get("age")
-
-        if password == repeat_password and int(age) >= 18 and username not in users:
-            return HttpResponse(f"Приветствуем, {username}!")
-        elif password != repeat_password:
-            info['error'] = 'Пароли не совпадают'
-        elif int(age) < 18:
-            info['error'] = 'Вы должны быть старше 18'
-        elif username in users:
-            info['error'] = 'Пользователь уже существует'
-
-    return render(request, 'fifth_task/registration_page.html', context=info)
-
-# def sign_up_by_django(request):
+# def sign_up_by_html(request):
 #     users = {
-#         'Ivan2': 'Ivan1999','Petr2': 'Petya2005'
+#         'Ivan': 'Ivan1999','Petr': 'Petya2005'
 #     }
 #     info = {
 #     }
-#     form = ContactForm()
 #     if request.method == "POST":
-#         form = ContactForm(request.POST)
+#         username = request.POST.get("username")
+#         password = request.POST.get("password")
+#         repeat_password = request.POST.get("repeat_password")
+#         age = request.POST.get("age")
 #
-#         if form.is_valid():
-#             username = form.cleaned_data("username")
-#             password = form.cleaned_data("password")
-#             repeat_password = form.cleaned_data("repeat_password")
-#             age = form.cleaned_data("age")
+#         if password == repeat_password and int(age) >= 18 and username not in users:
+#             return HttpResponse(f"Приветствуем, {username}!")
+#         elif password != repeat_password:
+#             info['error'] = 'Пароли не совпадают'
+#         elif int(age) < 18:
+#             info['error'] = 'Вы должны быть старше 18'
+#         elif username in users:
+#             info['error'] = 'Пользователь уже существует'
 #
-#             if password == repeat_password and int(age) >= 18 and username not in users:
-#                 return HttpResponse(f"Приветствуем, {username}!")
-#             elif password != repeat_password:
-#                 info['error'] = 'Пароли не совпадают'
-#             elif int(age) < 18:
-#                 info['error'] = 'Вы должны быть старше 18'
-#             elif username in users:
-#                 info['error'] = 'Пользователь уже существует'
-#     return render(request, 'fifth_task/registration_page.html', {'form': form})
+#     return render(request, 'fifth_task/registration_page.html', context=info)
+
+
+
+def sign_up_by_django(request):
+    users = {
+        'Ivan': 'Ivan00','Petr': 'Petr11'
+    }
+    info = {'error': None}
+    form = ContactForm()
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            if cd['password'] != cd['repeat_password']:
+                info['error'] = "Пароли не совпадают"
+            elif cd['age'] < 18:
+                info['error'] = "Вы должны быть старше 18"
+            elif cd["username"] in users:
+                info['error'] = "Пользователь уже существует"
+            else:
+                return HttpResponse(f"Приветствуем, {cd['username']}!")
+
+    info['form'] = form
+    return render(request, 'fifth_task/registration_page.html', info)
